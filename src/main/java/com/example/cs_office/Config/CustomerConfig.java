6d1a323cbf1e58fa27ff7cd557c41486572c0536
tranquocbao123 <baotqph09487@fpt.edu.vn>
2021-05-18 15:55:26 +0700

    Update CustomerConfig.java

--- src/main/java/com/example/cs_office/Config/CustomerConfig.java
+++ src/main/java/com/example/cs_office/Config/CustomerConfig.java
@@ -15,15 +15,17 @@ public class CustomerConfig {
     CommandLineRunner commandLineRunner(CustomerRepository repository){
         return args -> {
             Customer customer = new Customer(
-                    "0973718415", "bao@gmail.com", "bao123", "Bao", "Tran","80 Xuan Phuong", new Date(), true
+                     "bao@gmail.com"
             );
             Customer customer2 = new Customer(
                     4,"0969145287", "vannh@gmail.com", "bao123", "Bao", "Tran","80 Xuan Phuong", new Date(), true
             );
-            repository.findCustomerById(4);
-            repository.saveAll(List.of(customer));
+
+//            repository.findCustomerById(4);
+//            repository.saveAll(List.of(customer));
 //            repository.saveAndFlush(customer2);
 //            repository.deleteById(2);
+            repository.findCustomerByEmail(customer.getEmail());
         };
     }
 }
