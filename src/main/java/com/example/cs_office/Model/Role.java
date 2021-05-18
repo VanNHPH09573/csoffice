7cd9131da0fd23453b57a5f017609a206e698c14
tranquocbao123 <baotqph09487@fpt.edu.vn>
2021-05-18 15:55:48 +0700

    Update Role.java

--- src/main/java/com/example/cs_office/Model/Role.java
+++ src/main/java/com/example/cs_office/Model/Role.java
@@ -1,5 +1,33 @@
 package com.example.cs_office.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "Role")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    private String name;
+    @Column(name = "create_Date")
+    private Date createDate;
+    private boolean status;
+
+    public Role(String name, Date createDate, boolean status) {
+        this.name = name;
+        this.createDate = createDate;
+        this.status = status;
+    }
+
 }
