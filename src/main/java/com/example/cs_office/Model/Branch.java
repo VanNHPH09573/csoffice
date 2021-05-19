af31cecd33ff96311aeb1dcf4f67ad729d377a33
VanNHPH09573 <vannhph09573@gmail.com>
2021-05-19 14:58:09 +0700

    Update Branch.java

--- src/main/java/com/example/cs_office/Model/Branch.java
+++ src/main/java/com/example/cs_office/Model/Branch.java
@@ -4,8 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.Date;
 
 @Getter
@@ -14,16 +16,24 @@ import java.util.Date;
 @AllArgsConstructor
 
 @Entity
-@Table(name = "Branch")
+@Table(name = "branch")
 public class Branch {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private int id;
     private String name;
     private String address;
     @Column(name = "create_Date")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "MM/dd/yyyy")
     private Date createDate;
     private boolean status;
+
+    @OneToMany(mappedBy = "branch",fetch = FetchType.EAGER)
+    private Collection<Room> rooms;
+
+    @OneToMany(mappedBy = "branch1",fetch = FetchType.EAGER)
+    private Collection<Staff> staff;
 
     public Branch(String name, String address, Date createDate, boolean status) {
         this.name = name;
