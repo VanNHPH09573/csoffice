495681a0c233056a204ba7f442d52aa63752741d
VanNHPH09573 <vannhph09573@gmail.com>
2021-05-19 15:25:48 +0700

    Update DateMaster.java

--- src/main/java/com/example/cs_office/Model/DateMaster.java
+++ src/main/java/com/example/cs_office/Model/DateMaster.java
@@ -1,12 +1,20 @@
 package com.example.cs_office.Model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.util.Date;
 
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "datemaster")
 public class DateMaster {
@@ -22,4 +30,11 @@ public class DateMaster {
     @DateTimeFormat(pattern = "MM/dd/yyyy")
     private Date createDate;
     private boolean status;
+
+    public DateMaster(String desc, Date date, Date createDate, boolean status) {
+        this.desc = desc;
+        this.date = date;
+        this.createDate = createDate;
+        this.status = status;
+    }
 }
