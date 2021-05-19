25b419c6c172bf001c3ecaae7efaf9546617d3ee
VanNHPH09573 <vannhph09573@gmail.com>
2021-05-19 14:58:45 +0700

    Create Service.java

---
+++ src/main/java/com/example/cs_office/Model/Service.java
@@ -0,0 +1,42 @@
+package com.example.cs_office.Model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Date;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name="service")
+public class Service {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    @Column(name="id_PriceService")
+    private String idPriceService;
+    @Column(name="name")
+    private String nameService;
+    @Column(name = "create_Date")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "MM/dd/yyyy")
+    private Date createDate;
+    private boolean status;
+
+    @OneToMany(mappedBy="service", fetch=FetchType.EAGER)
+    private Collection<PriceService> priceServices;
+
+    @OneToMany(mappedBy = "service1",fetch = FetchType.EAGER)
+    private  Collection<ServiceDetail> serviceDetails;
+}
