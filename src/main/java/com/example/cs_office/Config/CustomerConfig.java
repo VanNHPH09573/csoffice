6a635ca4e9cee92d0f38156d3a59bd430d1ca7a9
tranquocbao123 <baotqph09487@fpt.edu.vn>
2021-05-19 09:22:03 +0700

    Update CustomerConfig.java

--- src/main/java/com/example/cs_office/Config/CustomerConfig.java
+++ src/main/java/com/example/cs_office/Config/CustomerConfig.java
@@ -9,23 +9,23 @@ import org.springframework.context.annotation.Configuration;
 import java.util.Date;
 import java.util.List;
 
-@Configuration
+//@Configuration
 public class CustomerConfig {
-    @Bean
-    CommandLineRunner commandLineRunner(CustomerRepository repository){
-        return args -> {
-            Customer customer = new Customer(
-                     "bao@gmail.com"
-            );
-            Customer customer2 = new Customer(
-                    4,"0969145287", "vannh@gmail.com", "bao123", "Bao", "Tran","80 Xuan Phuong", new Date(), true
-            );
+//    @Bean
+//    CommandLineRunner commandLineRunner(CustomerRepository repository){
+//        return args -> {
+//            Customer customer = new Customer(
+//                     "bao@gmail.com"
+//            );
+//            Customer customer2 = new Customer(
+//                    4,"0969145287", "vannh@gmail.com", "bao123", "Bao", "Tran","80 Xuan Phuong", new Date(), true
+//            );
 
 //            repository.findCustomerById(4);
 //            repository.saveAll(List.of(customer));
 //            repository.saveAndFlush(customer2);
 //            repository.deleteById(2);
-            repository.findCustomerByEmail(customer.getEmail());
-        };
-    }
+//            repository.findCustomerByEmail(customer.getEmail());
+//        };
+//    }
 }
