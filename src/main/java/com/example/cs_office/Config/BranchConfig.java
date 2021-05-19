7310d342a348d8f6876103fbb3a9b8d40f0d112f
VanNHPH09573 <vannhph09573@gmail.com>
2021-05-19 15:23:25 +0700

    Update BranchConfig.java

--- src/main/java/com/example/cs_office/Config/BranchConfig.java
+++ src/main/java/com/example/cs_office/Config/BranchConfig.java
@@ -9,7 +9,13 @@ import org.springframework.context.annotation.Configuration;
 import java.util.Date;
 import java.util.List;
 
-@Configuration
+//@Configuration
 public class BranchConfig {
-
+//    @Bean
+//    CommandLineRunner commandLineRunner(BranchRepotitory branchRepotitory){
+//        return args -> {
+//            Branch branch = new Branch(1,"Thanh Xuan","98 Thanh Xuan",new Date(), true);
+//            branchRepotitory.saveAll(List.of(branch));
+//        };
+//    }
 }
