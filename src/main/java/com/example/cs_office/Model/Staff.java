6212f014a5fde8c781ecfebef4f352f270f2a774
VanNHPH09573 <vannhph09573@gmail.com>
2021-05-19 14:58:58 +0700

    Update Staff.java

--- src/main/java/com/example/cs_office/Model/Staff.java
+++ src/main/java/com/example/cs_office/Model/Staff.java
@@ -1,6 +1,46 @@
 package com.example.cs_office.Model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "staff")
 public class Staff {
+    @Id
+    @GeneratedValue
     private int id;
+    @Column(name = "user_name")
+    private String userName;
+    @Column(name = "pass_word")
+    private String passWord;
+    @Column(name = "phone_Number")
+    private String phoneNumber;
+    private String email;
+    private String address;
+    @Column(name = "create_Date")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "MM/dd/yyyy")
+    private Date createDate;
+    private boolean status;
+
+    @ManyToOne
+    @JoinColumn(name="id_Role")
+    private Role role;
+
+    @ManyToOne
+    @JoinColumn(name="id_Branch")
+    private Branch branch1;
 
 }
