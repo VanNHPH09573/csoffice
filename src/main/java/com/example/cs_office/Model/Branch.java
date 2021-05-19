eba730227a2f9dd357c693d0c726917a00d7d6cf
tranquocbao123 <baotqph09487@fpt.edu.vn>
2021-05-19 09:22:13 +0700

    Update Branch.java

--- src/main/java/com/example/cs_office/Model/Branch.java
+++ src/main/java/com/example/cs_office/Model/Branch.java
@@ -1,5 +1,34 @@
 package com.example.cs_office.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "Branch")
 public class Branch {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    private String name;
+    private String address;
+    @Column(name = "create_Date")
+    private Date createDate;
+    private boolean status;
+
+    public Branch(String name, String address, Date createDate, boolean status) {
+        this.name = name;
+        this.address = address;
+        this.createDate = createDate;
+        this.status = status;
+    }
 }
