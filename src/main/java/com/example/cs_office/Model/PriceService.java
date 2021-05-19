dbcfc1e4d8766acd79c170bf522135c642e62d72
VanNHPH09573 <vannhph09573@gmail.com>
2021-05-19 14:58:28 +0700

    Create PriceService.java

---
+++ src/main/java/com/example/cs_office/Model/PriceService.java
@@ -0,0 +1,36 @@
+package com.example.cs_office.Model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name="priceService")
+public class PriceService {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private double value;
+    @Column(name = "create_Date")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "MM/dd/yyyy")
+    private Date createDate;
+    private boolean status;
+
+    @ManyToMany
+    @JoinColumn(name="id_Service")
+    private Service service;
+
+}
